e2f1873adb1389f7600578366d82f8c6e9c9d9e9
Karthik G <68554363+lelouch248@users.noreply.github.com>
2022-06-18 07:52:31 +0530

    Add files via upload

---
+++ 10_checker_board(8x8).py
@@ -0,0 +1,22 @@
+# 10 checker Board
+
+import numpy as np
+import cv2
+
+img = np.zeros((800, 800, 3))
+# img[y coordinates,x coordinates]
+for y in range(0, 710, 100):
+    if (y // 100) % 2 == 0:
+        count = 0
+    else:
+        count = 1
+    for x in range(0, 710, 100):
+        if (x // 100) % 2 == 0 and count == 0:
+            img[x:x + 100, y:y + 100] = 255, 255, 255
+
+        if (x // 100) % 2 == 1 and count == 1:
+            img[x:x + 100, y:y + 100] = 255, 255, 255
+
+cv2.imshow('checker board', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
